5c14e7ef90c61ee239e6792b62effb254b41fd0d
KEBOAIBEK <oybek.ayubov.1306@gmail.com>
2022-05-09 11:59:45 +0500

    Ma'lumot ombori

---
+++ Js/main.js
@@ -0,0 +1,8 @@
+var info = {}
+
+info.FullName = prompt('To`liq ism familiyangizni kiriting:' )
+info.study = prompt('Qaysi universitetda oqiysiz')
+info.language = prompt('nechta til bilasiz')
+info.color = prompt ('Yoqtirgan rangiz')
+
+console.log("Siz haqingizda batafsil",info)
